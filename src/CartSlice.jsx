678867f376ba6dbf08b34e6090a997c2733b8e65
renvis182 <renvis182@hotmail.com>
2025-12-09 14:25:04 -0500

    Adding new feature for the app on 20251209_1424

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,7 +7,14 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const handleAddToCart = (product) => {
+        dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
+
+        setAddedToCart((prevState) => ({ // Update the local state to reflect that the product has been added
+            ...prevState, // Spread the previous state to retain existing entries
+            [product.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
+        }));
+      };
     },
     removeItem: (state, action) => {
     },
