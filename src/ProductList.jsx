574f8142fbd06adf2d7c2703d3c901112b57cb42
renvis182 <renvis182@hotmail.com>
2025-12-10 16:02:46 -0500

    Adding new feature for the app on 20251210_1602

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addItem } from './CartSlice';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
     const dispatch = useDispatch(); 
-
+    const cartItems = useSelector(state => state.cart.items);
+    const calculateTotalQuantity = () => {
+        return CartItems ? CartItems.reduce((total, item) => total + item.quantity, 0) : 0;
+    };
     const plantsArray = [
         {
             category: "Air Purifying Plants",
