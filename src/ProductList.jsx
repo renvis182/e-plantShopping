ec6bb91b0ecd6609527fef729b839a486425532f
renvis182 <renvis182@hotmail.com>
2025-12-10 17:28:04 -0500

    Disabling add to cart on 20251210_1727

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -323,10 +323,11 @@ function ProductList({ onHomeClick }) {
                                 <div className="product-description">{plant.description}</div> {/* Display plant description */}
                                 <div className="product-cost">{plant.cost}</div> {/* Display plant cost */}
                                 <button
-                                    className="product-button"
-                                    onClick={() => handleAddToCart(plant)} // Handle adding plant to cart
-                                >
-                                    Add to Cart
+                                    className={`product-button ${addedToCart[plant.name] ? "added-to-cart" : ""}`}
+                                    onClick={() => handleAddToCart(plant)}
+                                    disabled={!!addedToCart[plant.name]}
+                                    >
+                                    {addedToCart[plant.name] ? "Added to Cart" : "Add to Cart"}
                                 </button>
                                 </div>
                             ))}
